1830d505476b0ed38dddc8df4593f01b1acdc114
Aman Tyagi <aman@playo.co>
2025-04-04 13:21:11 +0530

    learn about arrays in go

--- 2_simplevalues/main.go
+++ 2_simplevalues/main.go
@@ -1,4 +1,3 @@
 // in this he only explained the values that are there in this programming language
 
 package main
-
---
+++ 8_arrays/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    // var nums [4]int;
+    // nums[0] = 78;
+    // declare in one line
+    nums := [4]int{1,2,3,4} 
+    fmt.Println(nums)
+}
+
