a5db2daf30d8235610ad47337696ac68b1d405f2
Aman Tyagi <aman@playo.co>
2025-04-06 14:34:04 +0530

    update the slice function

--- 8_arrays/main.go
+++ 8_arrays/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    // var nums [4]int;
-    // nums[0] = 78;
-    // declare in one line
-    nums := [4]int{1,2,3,4} 
-    fmt.Println(nums)
+	// var nums [4]int;
+	// nums[0] = 78;
+	// declare in one line
+	// nums := [4]int{1,2,3,4}
+	// there is no need to specify the size of the array
+	nums := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(nums)
 }
-
---
+++ 9_slices/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr2 []int
+
+	arr := make([]int, 5, 10)
+	fmt.Println(arr, arr2)
+}
+
